2196f7ca446ac88e55ce8655280780c22150a8f4
Evren Tan <evrentan@gmail.com>
2022-02-01 10:24:59 +0300

    feat: Add Context Load Tests
    feat: Add Context Load Tests

--- service-discovery/src/test/java/evrentan/restaurantmanagement/servicediscovery/ServiceDiscoveryApplicationTests.java
+++ service-discovery/src/test/java/evrentan/restaurantmanagement/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -1,13 +1,27 @@
 package evrentan.restaurantmanagement.servicediscovery;
 
+import evrentan.restaurantmanagement.servicediscovery.spring.spring.ServiceDiscoveryApplication;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+import java.util.Objects;
+
+@SpringBootTest(classes = {ServiceDiscoveryApplication.class})
 class ServiceDiscoveryApplicationTests {
 
+  @Mock
+  private ApplicationContext applicationContext;
+
   @Test
+  @DisplayName("Context Load Testing")
   void contextLoads() {
+    Mockito.when(Objects.nonNull(this.applicationContext.getBean(ServiceDiscoveryApplication.class))).thenReturn(true);
+    Assertions.assertTrue(Objects.nonNull(this.applicationContext.getBean(ServiceDiscoveryApplication.class)));
   }
 
 }
--- spring-cloud-config-server/src/test/java/evrentan/restaurantmanagement/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
+++ spring-cloud-config-server/src/test/java/evrentan/restaurantmanagement/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -1,13 +1,27 @@
 package evrentan.restaurantmanagement.springcloudconfigserver;
 
+import evrentan.restaurantmanagement.springcloudconfigserver.spring.spring.SpringCloudConfigServerApplication;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+import java.util.Objects;
+
+@SpringBootTest(classes = {SpringCloudConfigServerApplication.class})
 class SpringCloudConfigServerApplicationTests {
 
+  @Mock
+  private ApplicationContext applicationContext;
+
   @Test
+  @DisplayName("Context Load Testing")
   void contextLoads() {
+    Mockito.when(Objects.nonNull(this.applicationContext.getBean(SpringCloudConfigServerApplication.class))).thenReturn(true);
+    Assertions.assertTrue(Objects.nonNull(this.applicationContext.getBean(SpringCloudConfigServerApplication.class)));
   }
 
 }
